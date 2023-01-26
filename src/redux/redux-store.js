79d0d7bd298cffaa5a6ae028a81330519b89dce8
Mate <mate.zakariadze@gmail.com>
2023-01-26 21:59:55 +0400

    add extensions
    redux and react dev tools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, legacy_createStore as createStore} from "redux";
 import cargoReducer from "./cargo-reducer";
 import loginReducer from "./login-reducer";
 import usersReducer from "./users-reducer";
@@ -16,7 +16,21 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers =
+    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
+        })
+        : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk)
+    // other store enhancers if any
+);
+const store = createStore(reducers, enhancer);
+
+// let store = createStore(reducers, applyMiddleware(thunk));
+
 
 window.store = store;
 
